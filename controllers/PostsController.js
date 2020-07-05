c03c42da40dcb6dec97626d742149e406da565b3
dbacall <dbacall@hotmail.co.uk>
2020-07-05 11:31:00 +0100

    add postcontroller with create function

---
+++ controllers/PostsController.js
@@ -0,0 +1,18 @@
+var Post = require('../models/post');
+
+var PostsController = {
+  create: (req, res) => {
+    var post = new Post(req.body);
+
+    post
+      .save()
+      .then((res) =>
+        res.status(200).json({ message: 'Post successfully added to database' })
+      )
+      .catch((res) =>
+        res.status(400).json({ error: 'Post could not be added to database.' })
+      );
+  },
+};
+
+module.exports = PostsController;
